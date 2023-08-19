fd1bdc7527b25a7dfcab3c10461d3a8b7377cab9
bruno.diaz04@alumnos.ucn.cl <bruno.diaz04@alumnos.ucn.cl>
2023-08-18 22:18:13 -0400

    a

--- web.py
+++ web.py
@@ -1,16 +1,42 @@
-#cada ambiente parte vacio
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("UNI_CORR_500_01.txt", 
+                 skiprows=4, 
+                 sep="\t", 
+                 names=["ID", "frames", "X", "Y", "Z"])
+    
+st.write("""
+# Mi primera aplicación interactiva
+## Histograma sobre el eje X e Y
+""")
 
-data=pd.read_csv("UNI_CORR_500_01.txt", skiprows=3,sep="\t") #names= cuando la tabla no tiene un encabezado 
-data["xpixel"]=round((data["X"])*35.6)+320
-data["ypixel"]=round(data["Y"]*(-96))+480
-fig, ax=plt.subplots()
-ax.hist2d(data["xpixel"],data["ypixel"],bins=(60,50),cmap="inferno")
-ax.set_title('Mapa de calor experimento 1', loc = "center")
-ax.set_xlabel("Largo pasillo")
-ax.set_ylabel("Ancho pasillo")
+# Using "with" notation
+with st.sidebar:
+    # Titulo
+    st.write("# Opciones")
+    # slider
+    div = st.slider('Número de bins:', 0, 130, 25)
+    st.write("Bins=", div)
 
+# Desplegamos un histograma con los datos del eje X
+fig, ax = plt.subplots(1, 2, figsize=(10, 3))
+ax[0].hist(df["X"], bins=div, color= "lightcoral")
+ax[0].set_xlabel('Posición en metros')
+ax[0].set_ylabel('Frecuencia')
+ax[0].set_title('Histograma posiciónes en el eje X')
+
+ax[1].hist(df["Y"], bins=div, color= "indianred")
+ax[1].set_xlabel('Posición en metros')
+ax[1].set_ylabel('Frecuencia')
+ax[1].set_title('Histograma posiciónes en el eje Y')
+
+# Desplegamos el gráfico
 st.pyplot(fig)
+
+st.write("""
+## Muestra de datos cargados
+""")
+# Graficamos una tabla
+st.table(df.head())
